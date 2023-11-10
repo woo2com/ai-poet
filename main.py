2a26579c43775a7f9fd0c975be91ff6d15eb6b3d
woo2com <51962364+woo2com@users.noreply.github.com>
2023-11-10 11:43:20 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# from dotenv import load_dotenv
+# load_dotenv()
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
+
+chat_model = ChatOpenAI()
+
+st.title('인공지능 시인')
+
+content = st.text_input('시의 주제를 제시해 주세요', '')
+
+if st.button('시 작성 요청하기', type="primary"):
+    with st.spinner('시 작성중'):
+        result = chat_model.predict(content + "에 대한 3줄 시를 작성해줘")
+        st.write(result)
+
+
+
+
+
+
+
